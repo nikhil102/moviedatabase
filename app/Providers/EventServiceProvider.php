0701d28300b8bd927e40b15e683c8ca9473b5253
nikhil102 <shindenikhil102@gmail.com>
2022-07-13 01:49:07 +0530

    movie project commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Common\Auth\Events\UserCreated' => [
+            'App\Listeners\CreateWatchlist',
+        ],
+        'Common\Auth\Events\UsersDeleted' => ['App\Listeners\DeleteUserLists',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
